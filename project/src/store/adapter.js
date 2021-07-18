dbfa22d14b481032e66e33a04af6848ec5af3bf9
Evgenii Popov <jekson87@me.com>
2021-07-18 11:23:16 +0300

    Исправляет адаптер для фильма

--- project/src/store/adapter.js
+++ project/src/store/adapter.js
@@ -1,32 +1,23 @@
 export const Adapter = {
-  adaptMovieToClient: (movie) => {
-    const newMovie = {
-      ...movie,
-      posterImage: movie.poster_image,
-      previewImage: movie.preview_image,
-      backgroundImage: movie.background_image,
-      backgroundColor: movie.background_color,
-      videoLink: movie.video_link,
-      previewVideoLink: movie.preview_video_link,
-      scoresCount: movie.scores_count,
-      actors: movie.starring,
-      runtime: movie.run_time,
-      isFavorite: movie.is_favorite,
-    };
-
-    delete newMovie.poster_image;
-    delete newMovie.preview_image;
-    delete newMovie.background_image;
-    delete newMovie.background_color;
-    delete newMovie.video_link;
-    delete newMovie.preview_video_link;
-    delete newMovie.scores_count;
-    delete newMovie.starring;
-    delete newMovie.run_time;
-    delete newMovie.is_favorite;
-
-    return newMovie;
-  },
+  adaptMovieToClient: (movie) => ({
+    id: movie.id,
+    name: movie.name,
+    posterImage: movie.poster_image,
+    previewImage: movie.preview_image,
+    backgroundImage: movie.background_image,
+    backgroundColor: movie.background_color,
+    videoLink: movie.video_link,
+    previewVideoLink: movie.preview_video_link,
+    description: movie.description,
+    rating: movie.rating,
+    scoresCount: movie.scores_count,
+    director: movie.director,
+    actors: movie.starring,
+    runtime: movie.run_time,
+    genre: movie.genre,
+    released: movie.released,
+    isFavorite: movie.is_favorite,
+  }),
   adaptCommentToClient: (comment) => ({
     ...comment,
     date: comment.date !== null
